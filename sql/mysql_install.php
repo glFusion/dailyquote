a2f68c0bfc39324e58d7fd5c2c5e908af0759bbb
Lee Garner <lee@leegarner.com>
2016-12-02 13:55:08 -0800

    Ensure consistent schema in prod and submission tables

--- sql/mysql_install.php
+++ sql/mysql_install.php
@@ -5,64 +5,63 @@
 *   @author     Lee Garner <lee@leegarner.com>
 *   @copyright  Copyright (c) 2009 Lee Garner <lee@leegarner.com>
 *   @package    dailyquote
-*   @version    0.0.1
-*   @license    http://opensource.org/licenses/gpl-2.0.php 
+*   @version    0.2.0
+*   @license    http://opensource.org/licenses/gpl-2.0.php
 *               GNU Public License v2 or later
 *   @filesource
 */
 
 $_SQL = array();
 
-// Main quote table
-$_SQL['dailyquote_quotes'] = 
-  "CREATE TABLE {$_TABLES['dailyquote_quotes']} (
-  id VARCHAR(40) NOT NULL PRIMARY KEY,
+$quote_table_creation = "id VARCHAR(40) NOT NULL PRIMARY KEY,
   quote TEXT,
   quoted TEXT,
   title TEXT,
   source TEXT,
   sourcedate VARCHAR(16),
-  dt INT(11) DEFAULT 0,
-  uid INT NOT NULL default '1',
-  enabled TINYINT(1) NOT NULL DEFAULT '1',
-  UNIQUE idx_quote (`quote`(32))
+  dt INT(11) UNSIGNED DEFAULT 0,
+  uid INT(11) UNSIGNED NOT NULL default '1',
+  enabled TINYINT(1) UNSIGNED NOT NULL DEFAULT '1',
+  UNIQUE idx_quote (`quote`(32))";
+
+// Main quote table
+$_SQL['dailyquote_quotes'] = "CREATE TABLE {$_TABLES['dailyquote_quotes']} (
+  $quote_table_creation
 )";
 
 // Submission Table
-$_SQL['dailyquote_submission'] = 
-  "CREATE TABLE {$_TABLES['dailyquote_submission']} (
-  id VARCHAR(40) NOT NULL PRIMARY KEY,
-  quote TEXT,
-  quoted TEXT,
-  title TEXT,
-  source TEXT,
-  sourcedate VARCHAR(16),
-  dt INT(11) DEFAULT 0,
-  uid INT NOT NULL default '1',
-  UNIQUE idx_quote (`quote`(32))
+$_SQL['dailyquote_submission'] = "CREATE TABLE {$_TABLES['dailyquote_submission']} (
+  $quote_table_creation
 )";
 
 // Categories Table
-$_SQL['dailyquote_cat'] = 
+$_SQL['dailyquote_cat'] =
   "CREATE TABLE {$_TABLES['dailyquote_cat']} (
   id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(64) NOT NULL,
-  enabled TINYINT(1) NOT NULL default '1',
+  enabled TINYINT(1) UNSIGNED NOT NULL default '1',
   UNIQUE idx_name (`name`(10))
 )";
 
 // Lookup Table
-$_SQL['dailyquote_quoteXcat'] = 
+$_SQL['dailyquote_quoteXcat'] =
   "CREATE TABLE {$_TABLES['dailyquote_quoteXcat']} (
   qid VARCHAR(40) NOT NULL,
-  cid INT(11) NOT NULL,
+  cid INT(11) UNSIGNED NOT NULL,
   PRIMARY KEY(qid,cid)
 )";
 
 
 // Default data
-$_SQL['dq_cat_data'] = 
+$_SQL['dq_cat_data'] =
     "INSERT INTO {$_TABLES['dailyquote_cat']} (name, enabled)
         VALUES ('{$LANG_DQ['misc']}', '1');";
+
+$SQL_UPGRADE=array(
+    '0.2.0' => array(
+        "ALTER TABLE {$_TABLES['dailyquote_submission']}
+            ADD `enabled` tinyint(1) default 1",
+    ),
+);
 
 ?>
--- upgrade.inc.php
+++ upgrade.inc.php
@@ -11,9 +11,12 @@
 *   @filesource
 */
 
-// Required to get the config values
-global $_CONF, $_CONF_DQ;
+global $_CONF, $_CONF_DQ, $_DB_dbms;
 
+/** Include the default configuration values */
+require_once dirname(__FILE)) . '/install_defaults.php';
+/** Include the table creation strings */
+require_once dirname(__FILE)) . "/sql/{$_DB_dbms}_install.php";
 
 /**
 *   Perform the upgrade starting at the current version.
@@ -47,9 +50,46 @@ function DQ_do_upgrade($current_ver)
         if ($c->group_exists($_CONF_DQ['pi_name'])) {
             $c->del('anonview', $_CONF_DQ['pi_name']);
         }
+        $errror = DQ_do_upgrade_sql('0.2.0', $sql);
+        if ($error) return $error;
     }
         
-    return $error;
+    return $error;      // == 0 at this point
+}
+
+
+/**
+*   Actually perform any sql updates
+*
+*   @param  array   $sql        Array of SQL statement(s) to execute
+*   @return integer             0 for success, 1 for error
+*/
+function DQ_do_upgrade_sql($version='Undefined')
+{
+    global $_CONF_DQ, $_SQL_UPGRADE;
+
+    // We control this, so it shouldn't happen, but just to be safe...
+    if ($version == 'Undefined') {
+        COM_errorLog("Error updating {$_CONF_DQ['pi_name']} - Undefined Version");
+        return 1;
+    }
+
+    // If no sql statements passed in, return success
+    if (empty($_SQL_UPGRADE[$version]))
+        return 0;
+
+    // Execute SQL now to perform the upgrade
+    COM_errorLOG("--Updating DailyQuote Ads to version $version");
+    foreach ($_SQL_UPGRADE[$version] as $sql) {
+        COM_errorLOG("DailyQuote Plugin $version update: Executing SQL => $s");
+        DB_query($sql, '1');
+        if (DB_error()) {
+            COM_errorLog("SQL Error during DailyQuote plugin update",1);
+            return 1;
+            break;
+        }
+    }
+    return 0;
 }
 
 ?>
