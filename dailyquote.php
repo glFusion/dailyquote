80ddc3270f36680edf67c9ab0a151347593906a3
Lee Garner <lee@leegarner.com>
2020-09-02 15:22:47 -0700

    Set version

--- dailyquote.php
+++ dailyquote.php
@@ -3,9 +3,9 @@
  * Table names and other global configuraiton values.
  *
  * @author      Lee Garner <lee@leegarner.com>
- * @copyright   Copyright (c) 2009-2017 Lee Garner <lee@leegarner.com>
+ * @copyright   Copyright (c) 2009-2020 Lee Garner <lee@leegarner.com>
  * @package     dailyquote
- * @version     v0.2.1
+ * @version     v0.3.0
  * @license     http://opensource.org/licenses/gpl-2.0.php
  *              GNU Public License v2 or later
  * @filesource
@@ -25,9 +25,9 @@ $_TABLES['dailyquote_cat']         = $DQ_prefix . 'category';
 $_TABLES['dailyquote_quoteXcat']   = $DQ_prefix . 'quoteXcat';
 
 // Static configuration items
-$_CONF_DQ['pi_version'] = '0.2.1';
+$_CONF_DQ['pi_version'] = '0.3.0';
 $_CONF_DQ['pi_name'] = 'dailyquote';
-$_CONF_DQ['gl_version'] = '1.6.0';
+$_CONF_DQ['gl_version'] = '1.7.7';
 $_CONF_DQ['pi_url'] = 'http://www.leegarner.com';
 $_CONF_DQ['pi_display_name'] = 'Daily Quotes';
 
